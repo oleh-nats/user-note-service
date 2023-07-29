601e9f76d5079dfe13f5bea96daab2ed28d2b0ea
oleh-nats <olehnats6@gmail.com>
2023-07-29 16:08:28 +0300

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,42 @@
-use thiserror::Error;
+use actix::MailboxError;
+use actix_web::{http::header::ToStrError, HttpResponse, ResponseError};
+use backtrace::Backtrace;
 
-#[derive(Error, Debug)]
-pub enum DataStoreError {
-    #[error("data store disconnected")]
-    Disconnect(#[from] io::Error),
-    #[error("the data for key `{0}` is not available")]
-    Redaction(String),
-    #[error("invalid header (expected {expected:?}, found {found:?})")]
-    InvalidHeader {
-        expected: String,
-        found: String,
-    },
-    #[error("unknown data store error")]
-    Unknown,
+#[allow(clippy::enum_variant_names)]
+#[derive(thiserror::Error, Debug)]
+pub enum AppErrors {
+    #[error(transparent)]
+    MailboxError(#[from] MailboxError),
+
+    #[error(transparent)]
+    AnyhowError(#[from] anyhow::Error),
+
+    #[error(transparent)]
+    ToStrError(#[from] ToStrError),
+
+    #[error(transparent)]
+    FromHexError(#[from] rustc_hex::FromHexError),
+
+    #[error(transparent)]
+    RequestError(#[from] reqwest::Error),
+
+    #[error(transparent)]
+    UrlParseError(#[from] url::ParseError),
 }
+
+#[derive(serde::Serialize)]
+pub struct ErrorResponse {
+    pub message: String,
+}
+
+impl ResponseError for AppErrors {
+    fn error_response(&self) -> HttpResponse {
+        println!("Call stack:\n{:?}", Backtrace::new());
+
+        HttpResponse::InternalServerError().json(ErrorResponse {
+            message: self.to_string(),
+        })
+    }
+}
+
+pub type AppResult<T> = Result<T, AppErrors>;
